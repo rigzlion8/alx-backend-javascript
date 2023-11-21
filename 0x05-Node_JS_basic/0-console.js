d5d01baa983c9f0e68405c2f418c95dfd18ffa39
Reagan Musa <reagan.musav@gmail.com>
2023-11-21 13:44:11 +0000

    0-main.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * function that that prints in STDOUT the string argument.
+ * @param {String} message to display
+ * @author Reagan Musa <https://github.com/rigzlion8>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
