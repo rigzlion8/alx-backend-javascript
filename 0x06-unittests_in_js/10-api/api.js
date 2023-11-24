98bdcbc7179ef4005ba8e8f3d179adac696d44b2
Reagan Musa <reagan.musav@gmail.com>
2023-11-24 18:17:57 +0000

    10-api

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,17 +1,13 @@
 const express = require('express');
 
 const app = express();
-const port = 7865;
+const PORT = 7865;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.end('Welcome to the payment system');
-});
+app.get('/', (req, res) => res.send('Welcome to the payment system'));
 
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`);
-});
+app.get('/cart/:id([0-9]+)', (req, res) => res.send(`Payment methods for cart ${req.params.id}`));
 
 app.get('/available_payments', (req, res) => {
   const obj = {
@@ -28,6 +24,4 @@ app.post('/login', (req, res) => {
   res.end(`Welcome ${username}`);
 });
 
-app.listen(port, () => {
-  console.log('API available on localhost port 7865');
-});
+app.listen(PORT, () => console.log(`API available on localhost port ${PORT}`));
