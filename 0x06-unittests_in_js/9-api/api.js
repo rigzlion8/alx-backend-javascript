924182838e1a743a4cf7dedb54a5d4ea9a388d6e
Reagan Musa <reagan.musav@gmail.com>
2023-11-24 18:00:48 +0000

    9-api

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 7865;
+
+app.get('/', (req, res) => res.send('Welcome to the payment system'));
+
+app.get('/cart/:id([0-9]+)', (req, res) => res.send(`Payment methods for cart ${req.params.id}`));
+
+app.listen(PORT, () => console.log(`API available on port ${port}`));
