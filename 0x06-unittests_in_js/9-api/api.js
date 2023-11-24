df682c65371b54347527b4a1547e993d2466a51c
Reagan Musa <reagan.musav@gmail.com>
2023-11-24 18:11:57 +0000

    hotfix-api.js-9-api

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -6,4 +6,4 @@ app.get('/', (req, res) => res.send('Welcome to the payment system'));
 
 app.get('/cart/:id([0-9]+)', (req, res) => res.send(`Payment methods for cart ${req.params.id}`));
 
-app.listen(PORT, () => console.log(`API available on port ${port}`));
+app.listen(PORT, () => console.log(`API available on port ${PORT}`));
